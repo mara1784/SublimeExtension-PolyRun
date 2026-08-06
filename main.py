91d66537ea232480ad8e5351047fe737ca82adc6
Marek Semerák <m.semerak@post.cz>
2026-08-06 20:57:16 +0000

    Create main

---
+++ main.py
@@ -0,0 +1,27 @@
+import sublime
+import sublime_plugin
+import subprocess
+import os
+
+class RunPythonScriptsInMoreTerminalsCommand(sublime_plugin.WindowCommand):
+    def run(self):
+        view = self.window.active_view() # current file on display
+
+        if view is None or view.file_name() is None:
+            sublime.error_message("Before first load you must save the file")
+            return
+
+        view.run_command("save")
+        file = view.file_name()
+
+        cmd = [
+            "x-terminal-emulator",
+            "-e",
+            "bash",
+            "-c",
+            f'python3 -u "{file}"; ' # there you can costumize languages that run scripts
+            'echo; '
+            'read -p "Press Enter for close..."'
+        ]
+
+        subprocess.Popen(cmd)
